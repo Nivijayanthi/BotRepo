a8bebf50cbb5827000dcdebb55eb5e3cd76c90a3
Nivetha K <39132@Hexaware.com>
2018-09-04 10:13:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var apiai = require('apiai');
+
+var appai = apiai("ee3683b183ec498ea5a1f277a85974fd");
+const util = require('util');
+
+var express=require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+//Assign port
+app.use(express.static(__dirname));
+
+//Configuring express app behaviour
+//imports
+
+app.post('/fulfillment',function(req,res){
+    debugger
+    var response;
+    console.log("request from dialogflow",req);
+    
+    if(req.queryResult.intent.displayName == 'CHANGE-RISK-PROFILE'){
+      var currentProfile = req.queryResult.parameters.CurrentProfile;
+      var targetProfie =  req.queryResult.parameters.TargetProfile;
+      var clientId = req.queryResult.parameters.ClientId;
+
+      if(clientId){        
+             console.log("currentProfile",currentProfile);
+          console.log("targetProfie",targetProfie);
+          console.log("clientId",clientId);
+          response = 'Please find the fund details';
+      }
+
+    }   
+
+    return res.json({
+        speech:response,
+        displayText:response,
+        source:'portal',
+    });  
+        });
+
+console.log("Server Running at Port : " + port);
+
+app.listen(port);
+       
+            
+  
+// app.listen(portC, function(){
+//     console.log('Listening my app on  PORT: ' + portC);
+// });
+        

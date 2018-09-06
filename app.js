725ea90eeacd57ad1f3821ce947d8bd5ad8aea7b
Nivetha K <39132@Hexaware.com>
2018-09-06 14:57:21 +0530

    response

--- app.js
+++ app.js
@@ -162,6 +162,19 @@ app.post('/fulfillment', async function (req, res) {
             });
 
     }
+    if(req.body.result.metadata.intentName == 'CURRENT-RISK-PROFILE'){
+        var clientId = req.body.result.parameters.clientId;
+        await query.ClientRiskProfileGet({ ClientID: clientId, Active: 'Y' }).then(function (data) {
+            console.log("The response from DB risk profile..............", JSON.stringify(data));
+            val = data.RiskCategory;
+        });
+        response = `Your current risk profile is ${val}`;
+         return res.json({
+                speech: response,
+                displayText: response,
+                source: 'portal',
+            });
+    }
 })
 console.log("Server Running at Port : " + port);
 

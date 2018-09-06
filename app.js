c08257c390443ed0fc9e8c96ac3705fd1910e8f2
Nivetha K <39132@Hexaware.com>
2018-09-06 14:32:55 +0530

    response

--- app.js
+++ app.js
@@ -144,10 +144,10 @@ app.post('/fulfillment', async function (req, res) {
             console.log("Inside change");
              var currentProfile =req.body.result.contexts[0].parameters.CurrentProfile;
              var targetProfile = req.body.result.contexts[0].parameters.TargetProfile;
-            response = `Your change request for risk category has been sent to the Trading desk. You will be receiving a detailed  email shortly. \n Current risk profile : ${currentProfile} \n Target risk profile : ${targetProfile} \n Customer Id : ${clientId} `;
+            response = `Your change request for risk category has been sent to the Trading desk. You will be receiving a detailed  email shortly. \n\n Current risk profile : ${currentProfile} \\n Target risk profile : ${targetProfile} \n\n Customer Id : ${clientId} `;
         }if(resType == 'add-fund-folowup'){
             console.log("Inside add");
-           response = `Your request to add new fund has been sent to the Trading desk. You will be receiving a detailed  email shortly. \n Customer Id : ${clientId}`;
+           response = `Your request to add new fund has been sent to the Trading desk. You will be receiving a detailed  email shortly. \n\n Customer Id : ${clientId}`;
         }      
          return res.json({
                 speech: response,

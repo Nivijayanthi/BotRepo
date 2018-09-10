ad9592a0c4eefec4deeb5f2d075dd7f80970bbb3
Nivetha K <39132@Hexaware.com>
2018-09-10 13:22:32 +0530

    auth

--- app.js
+++ app.js
@@ -15,11 +15,7 @@ const query = require('./query');
 const template = require('./template');
 var mail = require('./SendEmail');
 var MicrosoftGraph = require("@microsoft/microsoft-graph-client");
-var AuthenticationContext = require('adal-node').AuthenticationContext;
-var oResource = 'https://graph.microsoft.com';
-var oClientId = process.env.oClientId;
-    var oClientSecret = process.env.oClientSecret;
-     var oAuthorityURL = process.env.oAuthorityURL;
+const authHelper = require('./auth');
 
 async function showListOfFunds(clientId, riskProfile) {
     console.log("I am inside show method");
@@ -48,6 +44,8 @@ async function showListOfFunds(clientId, riskProfile) {
 
 
 app.post('/fulfillment', async function (req, res) {
+
+    const code =req.query.code;
 
     var clientId =  req.body.sessionId.slice(-6);
     
@@ -174,6 +172,8 @@ app.post('/fulfillment', async function (req, res) {
     }
     if (req.body.result.metadata.intentName == 'CURRENT-RISK-PROFILE') {
 
+         token = await authHelper.getTokenFromCode(code);
+
         const mailBody =
             {
                 "message": {
@@ -205,28 +205,11 @@ app.post('/fulfillment', async function (req, res) {
             profile: {
                 oid: "1b02070e-606c-42df-b83d-1af09b29bb1f",
                 displayName: "Nivetha K",
-                accessToken: "'eyJ0eXAiOiJKV1QiLCJub25jZSI6IkFRQUJBQUFBQUFEWHpaM2lmci1HUmJEVDQ1ek5TRUZFWGdzRTZIRXdweWktNFB3dFotX2xqOGhSM1VjTUJZRjAtTjY3SHRyZkV4VXkyOE5ERzNXYmo3TGZONnhYdHRHY0Fwb0Y0djhpSTFpQ0l4R0ZyYXlHaGlBQSIsImFsZyI6IlJTMjU2IiwieDV0IjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3Iiwia2lkIjoiN19adWYxdHZrd0x4WWFIUzNxNmxValVZSUd3In0.eyJhdWQiOiJodHRwczovL2dyYXBoLm1pY3Jvc29mdC5jb20iLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC83YzBjMzZmNS1hZjgzLTRjMjQtODg0NC05OTYyZTAxNjM3MTkvIiwiaWF0IjoxNTM2NTU5MjcxLCJuYmYiOjE1MzY1NTkyNzEsImV4cCI6MTUzNjU2MzE3MSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IjQyQmdZREE5Zkc3ZXgzUUYzUzFNOWg4VW82dWNOWnRWTXczUE05MFVXL0xoMVpGTElUOEEiLCJhbXIiOlsid2lhIl0sImFwcF9kaXNwbGF5bmFtZSI6IkFsaWNlIiwiYXBwaWQiOiI0OGY0MDRiMS0yYjYyLTRlN2EtOGU2Ny05OGE1ZDcyZjM2MWMiLCJhcHBpZGFjciI6IjEiLCJmYW1pbHlfbmFtZSI6IksiLCJnaXZlbl9uYW1lIjoiTml2ZXRoYSIsImlwYWRkciI6IjE2NS4yMjUuMTA0Ljk2IiwibmFtZSI6Ik5pdmV0aGEgSyIsIm9pZCI6IjFiMDIwNzBlLTYwNmMtNDJkZi1iODNkLTFhZjA5YjI5YmIxZiIsIm9ucHJlbV9zaWQiOiJTLTEtNS0yMS0xNjQ0NDkxOTM3LTgxMzQ5NzcwMy02ODIwMDMzMzAtMTUzODg0IiwicGxhdGYiOiIzIiwicHVpZCI6IjEwMDNCRkZEQTVBQzQzQTQiLCJzY3AiOiJNYWlsLlNlbmQgb3BlbmlkIHByb2ZpbGUgVXNlci5SZWFkIGVtYWlsIiwic3ViIjoiSUNUMFdnaG9CZXFUS0NQb0FHTjYxRHBtbWZNUmYtRlhHUXB3S1hibTJLayIsInRpZCI6IjdjMGMzNmY1LWFmODMtNGMyNC04ODQ0LTk5NjJlMDE2MzcxOSIsInVuaXF1ZV9uYW1lIjoiMzkxMzJASGV4YXdhcmUuY29tIiwidXBuIjoiMzkxMzJASGV4YXdhcmUuY29tIiwidXRpIjoiODhVYWVLQ1p6VTJldTRBV1JzMFFBQSIsInZlciI6IjEuMCIsInhtc19zdCI6eyJzdWIiOiJfV1ZWbjdFbnRCS0xkTU9aOGk0bGJ6QmVrWFVkaElobFFwU0JudGFKX2Q0In19.iq2n1lUIH9puA2T6RKvHlXQJE1mI4CaneFfwK-Vg92wZSe9pq8-MwhYlzKxQcOedGlo7COgAK4xontD1yU2F8n0h0oIevuQo9q5LH4LqrzBdIIj5W7c5VB_AMtNFS4R404V12x0vnoRAUx_38BfSs6TLfn4i1pZ_fHQhzPEGWaMX2gIxqUAHTAnkxT7O0AW1yExk2_92Kv5T0kGXBSfokaiIhlncsfrTIUPa85TC9pH7tWe3zlz6vEDQO1dC8VfxluCuLGIXklIdc273jkxS9zeb3a9KhSAtYgqFC0Bvei4yKPsUd6PCr0zpaGqfUuUS4IfJJ1f63-Vn1slwUyMjdw"
+                accessToken: null
             }
         };  
 
-        let context = new AuthenticationContext(oAuthorityURL);
-
-            context.acquireTokenWithClientCredentials(oResource, oClientId, oClientSecret,
-                function (err, tokenResponse) {
-                    if (err) {
-                        console.log("Access error. Are you logged into your Hexaware account?");
-                    } else {                        
-                        Token = tokenResponse.accessToken; 
-                        console.log("Nivetha", Token);                      
-                            MicrosoftGraphClient = MicrosoftGraph.Client.init({
-                                defaultVersion: 'v1.0',
-                                authProvider: (done) => {
-                                    done(null, Token); //first parameter takes an error if you can't get an access token 
-                                }
-                            }); 
-
-                    }
-                });
+        user.accessToken = token;
 
         mail.sendEmail(user, mailBody, function (response,err) {
            console.log("user1,,,,,,,,",JSON.stringify(user));
--- auth.js
+++ auth.js
@@ -1,39 +1,65 @@
-const graph = require('@microsoft/microsoft-graph-client');
-
-module.exports = {
-  getUserEmail: (user, done) => {
-    var client = graph.Client.init({
-      defaultVersion: 'v1.0',
-      debugLogging: true,
-      authProvider: function(authDone) {
-        authDone(null, user.accessToken);
-      }
-    });
-
-    client.api('/me').select(['mail', 'userPrincipalName']).get(
-      (err, me) => {
-        if (err) {
-          return done(err);
-        }
-
-        return done(null, me.mail ? me.mail : me.userPrincipalName);
-      }
-    );
-  },
-
-  sendEmail: (user, message, done) => {
-    var client = graph.Client.init({
-      defaultVersion: 'v1.0',
-      debugLogging: true,
-      authProvider: function(authDone) {
-        authDone(null, user.accessToken);
-      }
-    });
-
-    client.api('/me/sendmail').post(message,
-      (err) => {
-        return done(err);
-      }
-    );
-  }
+require('dotenv').config();
+const credentials = {
+    client: {
+        id: process.env.APP_ID,
+        secret: process.env.APP_PASSWORD,
+    },
+    auth: {
+        tokenHost: 'https://login.microsoftonline.com',
+        authorizePath: 'common/oauth2/v2.0/authorize',
+        tokenPath: 'common/oauth2/v2.0/token'
+    }
 };
+const oauth2 = require('simple-oauth2').create(credentials);
+function getAuthUrl() {
+    const returnVal = oauth2.authorizationCode.authorizeURL({
+        redirect_uri: process.env.REDIRECT_URI,
+        scope: process.env.APP_SCOPES
+    });
+    console.log(`Generated auth url: ${returnVal}`);
+    return returnVal;
+}
+async function getAccessToken(cookies, res) {
+    // Do we have an access token cached?
+    let token = cookies.graph_access_token;
+
+    if (token) {
+        // We have a token, but is it expired?
+        // Expire 5 minutes early to account for clock differences
+        const FIVE_MINUTES = 300000;
+        const expiration = new Date(parseFloat(cookies.graph_token_expires - FIVE_MINUTES));
+        if (expiration > new Date()) {
+            // Token is still good, just return it
+            return token;
+        }
+    }
+
+    // Either no token or it's expired, do we have a
+    // refresh token?
+    const refresh_token = cookies.graph_refresh_token;
+    if (refresh_token) {
+        const newToken = await oauth2.accessToken.create({
+            refresh_token: refresh_token
+        }).refresh();
+        saveValuesToCookie(newToken, res);
+        return newToken.token.access_token;
+    }
+
+    // Nothing in the cookies that helps, return empty
+    return null;
+}
+async function getTokenFromCode(auth_code) {
+    let result = await oauth2.authorizationCode.getToken({
+        code: auth_code,
+        redirect_uri: process.env.REDIRECT_URI,
+        scope: process.env.APP_SCOPES
+    });
+
+    const token = oauth2.accessToken.create(result);
+    console.log('Token created: ', token.token);
+    return token.token.access_token;
+}
+
+exports.getTokenFromCode = getTokenFromCode;
+exports.getAccessToken = getAccessToken;
+exports.getAuthUrl = getAuthUrl;

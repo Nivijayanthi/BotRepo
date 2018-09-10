6f284eadbb548abc25c0d04ec0d1923bde9f5efd
Srinivasan Velu <40140@hexaware.com>
2018-09-10 10:53:09 +0530

    new commit

--- app.js
+++ app.js
@@ -273,7 +273,7 @@ app.post('/fulfillment', async function (req, res) {
             let productName = funddetails[0].Name;
             await query.productperformanceGet({ ProductID: productID }).then(async function (product) {
                 console.log(productID + "=>" + clientId)
-                await query.holdingsProfileGet({ ProductID: productID, CustomerID: clientId }).then(function (holdingsd) {
+                await query.holdingsProfileGet({ ProductID: productID, CustomerID: clientId }).then(async function (holdingsd) {
                     if (product.length > 0 && holdingsd.length > 0) {
                         let currentPrice = product[0].Currentprice;
                         let quantity = holdingsd[0].Quantity;

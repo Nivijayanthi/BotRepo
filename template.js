7c2fe7136efefe55c620e6457cf502b188d31d9e
Nivetha K <39132@Hexaware.com>
2018-09-12 18:25:21 +0530

    master

--- template.js
+++ template.js
@@ -5,28 +5,58 @@ module.exports.CustomListTemplate = function () {
     var objectList = {
         "type": 1,
         "platform": "facebook",
-        "title" : null,
-        "subtitle" : null,
-        "buttons" : [{
-                "text": "Buy",
-                "postback": "Add new fund"
+        "title": null,
+        "subtitle": null,
+        "buttons": [{
+            "text": "Buy",
+            "postback": "Add new fund"
+        }]
 
-    }]
+    }
 
-}
-
-return objectList;
+    return objectList;
 }
 
 module.exports.QuickReplyTemplate = function () {
 
-    var objectList ={
-                    "content_type": "text",
-                    "title": null,
-                    "payload": "SEND-EMAIL"
-                    // "text": null,
-                    // "postback": "Add new fund"
-                }
+    var objectList = {
+        "content_type": "text",
+        "title": null,
+        "payload": null
+    }
+    return objectList;
+}
 
-return objectList;
+module.exports.TargetProfileSelectResponse = {
+    "facebook": {
+        "text": "Please choose the target risk category",
+        "quick_replies": [
+            {
+                "content_type": "text",
+                "title": "Growth",
+                "payload": "Growth"
+            },
+            {
+                "content_type": "text",
+                "title": "Adventurous",
+                "payload": "Adventurous"
+            },
+            {
+                "content_type": "text",
+                "title": "Moderate",
+                "payload": "Moderate"
+            }
+        ]
+    }
+}
+
+module.exports.eventCall ={
+  "followupEventInput": {
+    "name": null,
+    "parameters": {
+      "parameter-name-1": null,
+      "parameter-name-2": null
+    },
+    "languageCode": "en-US"
+  }
 }

9aa66281ca62bc6f18639dd672a5188fc44cd799
Nivetha K <39132@Hexaware.com>
2018-09-17 15:01:32 +0530

    followup

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ let replies = [];
                 console.log("current", JSON.stringify(currentProfile));
                 if (reply.title != currentProfile) {
                     console.log("reply.title", reply.title);
-                    replies.push(JSON.parse(JSON.stringify(reply)));
+                    replies.push(reply);
                 }
             console.log("&&&&&&&&&&", JSON.stringify(replies));
             });

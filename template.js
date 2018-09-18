3818cbdf8494b215086c6cb0b4a2f27134a96d42
Nivetha K <39132@Hexaware.com>
2018-09-18 10:51:57 +0530

    followup

--- template.js
+++ template.js
@@ -57,8 +57,8 @@ CurrentProfile: null,
 TargetProfile: null,
 ClientId : null
 }
-},
-contextOut: req.body.result.contexts
+}
+//contextOut: req.body.result.contexts
 }
 
 module.exports.CommonEventCall = {
